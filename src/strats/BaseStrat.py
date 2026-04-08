54e5dcba2ebb1818f39a068d2e7626dd1c28dc80
jaskirat <jaskiratsingh1208@gmail.com>
2026-04-08 00:19:13 -0300

    QQ-1: First strategy with historical MD simulation

--- src/strats/BaseStrat.py
+++ src/strats/BaseStrat.py
@@ -1,24 +1,53 @@
-from av_core import app
 from typing import Optional
 
-class BaseStrategy(app.App):
+from av_core.logger import LoggerConfig
+
+class OHLCQuote:
+    def __init__(self, symbol, timestamp, open_price, high_price, low_price, close_price, volume):
+        self.timestamp = timestamp
+        self.open_price = open_price
+        self.high_price = high_price
+        self.low_price = low_price
+        self.close_price = close_price
+        self.symbol = symbol
+        self.volume = volume
+
+    def __repr__(self):
+        return f"OHLCQuote(symbol={self.symbol}, timestamp={self.timestamp}, open={self.open_price}, high={self.high_price}, low={self.low_price}, close={self.close_price}, volume={self.volume})"
+
+class BaseStrategy:
     """
     Initializes the base strategy with a name and an optional configuration dictionary. The name is used to identify the strategy, while the configuration can hold any parameters or settings that the strategy may require.
     Args:        name (str): The name of the strategy.
         cfg (dict, optional): A dictionary containing configuration parameters for the strategy. Defaults to None.
     Depending on market data implementation, you can initialize  necessary market data subscriptions in the constructor and initite the market data feed. 
     """
-    def __init__(self, config_file: Optional[str] = None,  log_file: Optional[str] = None, instance_name: str = ""):
-        super().__init__(config_file=config_file, log_file=log_file, instance_name=instance_name)
+    def __init__(self, *args):
+        # if('name' in args):
+        #     self.name = args['name']
 
+        # self.name = type(self).__name__
+        # self.logger = LoggerConfig.setup(name=self.name)
+        pass
+
+    def load(self):
+        """
+        This method can be used to load any necessary resources or data for the strategy. 
+        Subclasses can override this method to provide specific loading behavior.
+        """
+        pass
+    
     """
-    This function should be called when new market data is received. Subclasses should implement this method to define how they process incoming market data.
+    This function should be called when new market data is received. S
+    ubclasses should implement this method to define how they process incoming market data.
     """
-    def on_md(self, md):
+    def on_md(self, md: OHLCQuote):
         raise NotImplementedError("on_md method must be implemented by subclasses.")
 
     def start(self):
         """
-        This method can be used to start the strategy, such as initializing any necessary resources or starting market data subscriptions. Subclasses can override this method to provide specific startup behavior.
+        This method can be used to start the strategy's main logic. 
+        Subclasses can override this method to provide specific behavior when the strategy is started.
         """
         pass
+
---
+++ src/strats/smartapi/BackTestedStrat.py
@@ -0,0 +1,80 @@
+
+from datetime import datetime
+from typing import Optional
+from av_core.logger import LoggerConfig
+from data.angel_one.app import AngelOneSmartApp
+from strats.BaseStrat import BaseStrategy, OHLCQuote
+
+class BackTestedStrat(BaseStrategy, AngelOneSmartApp):
+    def __init__(self, config_file: Optional[str] = None, log_file: Optional[str] = None, instance_name: str = ""):
+        AngelOneSmartApp.__init__(self, config_file=config_file, log_file=log_file, instance_name=instance_name)
+    
+    def init_strategy_params(self, symbols, start_date, end_date, interval):
+        super().__init__()
+        self.symbols = symbols
+        self.start_date = start_date
+        self.end_date = end_date
+        self.interval = interval
+        self.candle_info = {symbol: [] for symbol in symbols}
+
+        for symbol in symbols:
+            self.subscribe_md(symbol, start_date, end_date, self.interval)
+
+
+    def subscribe_md(self, symbol, start_date, end_date, interval):
+        # Implement logic to subscribe to market data for backtesting
+        candle_info_params = {
+            "exchange": "NSE",
+            "symboltoken": symbol,
+            "interval": interval,
+            "fromdate": start_date,
+            "todate": end_date
+        }
+        # Subscribe to market data using the parameters
+        self.logger.info("Fetching historical candle data for backtesting...")
+        candle_info = self.m_client.getCandleData(candle_info_params)
+        self.candle_info[symbol] += (candle_info['data'])
+        self.logger.info(f"Successfully fetched candle data for symbol: {symbol}")
+        if symbol not in self.candle_info:
+            self.candle_info[symbol] = []
+
+    def load(self):
+        """
+        Initialize subscribers. It calls the getCandleinfo API to fetch historical data for the 
+        specified symbols and stores it in the candle_info dictionary. This data will be used for 
+        backtesting the strategy.
+        """
+        # Implement logic to load any necessary resources or data for backtesting
+        for symbol in self.symbols:
+            self.subscribe_md(symbol, self.start_date, self.end_date, self.interval)
+
+    def on_md(self, md):
+        # Process market data for backtesting
+        print("Tick received", md)
+
+    def start(self):
+        """
+         Start the backtesting strategy. This method processes the historical market data stored
+         in the candle_info dictionary and plays it from start to end symbol-by-symbol.
+         
+         The method iterates through the market data for each symbol and calls the on_md method to
+         process the data. The backtesting continues until all the market data for all symbols 
+         has been processed.
+        """
+        self.logger.info("Starting backtesting strategy...")
+        finished = False
+        while not finished:
+            for symbol in self.symbols:
+                if symbol in self.candle_info and self.candle_info[symbol]:
+                    md = self.candle_info[symbol].pop(0)
+                    md_obj = OHLCQuote(symbol, md[0], md[1], md[2], md[3], md[4], md[5])
+                    self.on_md(md_obj)
+            if all(symbol not in self.candle_info or not self.candle_info[symbol] for symbol in self.symbols):
+                finished = True        
+
+
+if(__name__ == "__main__"):
+    app = BackTestedStrat()
+    app.init_strategy_params(["99926000", "99926001"], "2025-09-06 11:15", "2026-02-30 12:00", "ONE_MINUTE")
+    app.start()
+    # app.summary()
