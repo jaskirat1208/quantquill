411797188a9df614eb243dbf4e1e2d8c3711a2ca
jaskirat <jaskiratsingh1208@gmail.com>
2026-09-12 23:26:14 -0300

    QQ-3: Use configurable local instance config, use port 3002 for local tests

--- packages/alpha_server/start_alpha_server.py
+++ packages/alpha_server/start_alpha_server.py
@@ -1,13 +1,16 @@
 from quantquill import av_core
 from alpha_server.main import start_alpha_server
+import configparser
 
 
 class AlphaServer(av_core.app.App):
     def __init__(self):
-        pass
+        config = configparser.ConfigParser()
+        config.read('./configs/start_alpha_server.py.cnf')
+        self.port = config.getint('server', 'port', fallback=8091)
     
     def start(self):
-        start_alpha_server()
+        start_alpha_server(self.port)
 
 
 def main():
